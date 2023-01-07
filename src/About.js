4174a289b8408b10dadac89b855dea9e029b6bed
happyvenky <31536844+happyvenky@users.noreply.github.com>
2023-01-07 13:18:22 +0530

    Name & Goble Export

---
+++ src/About.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+/*export default function About() {
+  return (
+    <div>
+      <h1>Function -- Name Export working like Goble</h1>
+    </div>
+  );
+}*/
+
+export const About = () => {
+  return (
+    <div>
+      <h1>arrow Function -- Name Export</h1>
+    </div>
+  );
+};
+
+/*function About() {
+  return (
+    <div>
+      <h1>Goble Export</h1>
+    </div>
+  );
+}
+export default About;*/
--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import { About } from './About';
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+
+      <br />
+      <About />
     </div>
   );
 }
